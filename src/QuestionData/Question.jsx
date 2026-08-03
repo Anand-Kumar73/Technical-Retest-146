d1d7f8fa881d07c2c184f40c17f146d5f9a30bff
Anand kumar <anandk26771@gmail.com>
2026-08-03 15:55:28 +0530

    updated question part

---
+++ src/QuestionData/Question.jsx
@@ -0,0 +1,54 @@
+const questions = [
+  {
+    id: 1,
+    question: "What is React?",
+    answer: "React is a JavaScript library used to build user interfaces."
+  },
+  {
+    id: 2,
+    question: "What is JSX?",
+    answer: "JSX is a syntax extension of JavaScript."
+  },
+  {
+    id: 3,
+    question: "What is a Component?",
+    answer: "A Component is a reusable piece of UI."
+  },
+  {
+    id: 4,
+    question: "What are Props?",
+    answer: "Props are used to pass data from parent to child components."
+  },
+  {
+    id: 5,
+    question: "What is useState()?",
+    answer: "useState() is a Hook used to manage state."
+  },
+  {
+    id: 6,
+    question: "What is the purpose of React Router?",
+    answer: "It is used for navigation between pages."
+  },
+  {
+    id: 7,
+    question: "Why do we use map() in React?",
+    answer: "map() is used to render lists of components."
+  },
+  {
+    id: 8,
+    question: "Why is the key prop important while rendering a list?",
+    answer: "It helps React identify list items efficiently."
+  },
+  {
+    id: 9,
+    question: "What is Conditional Rendering?",
+    answer: "Showing different UI based on conditions."
+  },
+  {
+    id: 10,
+    question: "What is the difference between State and Props?",
+    answer: "State is mutable, Props are read-only."
+  }
+];
+
+export default questions;
